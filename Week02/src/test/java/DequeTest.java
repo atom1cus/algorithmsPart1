ea7ce621896008c124002ac433b778a2131ef01a
Artem Vegera <atomic.dreamer@gmail.com>
2015-03-11 18:23:35 +0300

    Added module for week02 tasks.

---
+++ Week02/src/test/java/DequeTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.assertNotNull;
+
+public class DequeTest {
+
+    @org.junit.Test
+    public void testDeque() throws Exception {
+        //TODO: write unit-tests
+        assertNotNull(new Deque<Object>());
+    }
+}
